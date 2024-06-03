cfd82adad5a06493ca6156ebf1fdecc09d1d9582
Sc0ld <58890206+Sc0ld@users.noreply.github.com>
2024-06-03 10:14:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ app.secret_key = "manbearpig_MUDMAN888"
 
 @app.route("/hello")
 def index():
-	flash("what's your name?")
-	return render_template("index.html")
+    flash("what's your name?")
+    return render_template("index.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
